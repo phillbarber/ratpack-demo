3264104648b9b3bc6abcbc1c2a488191a33c26dc
pergola <philliprbarber@googlemail.com>
2016-02-08 17:39:55 +0000

    minor format improvement

--- src/main/java/com/github/phillbarber/scenario/doubleobservable/DoubleObservableService.java
+++ src/main/java/com/github/phillbarber/scenario/doubleobservable/DoubleObservableService.java
@@ -5,25 +5,7 @@ import rx.Subscriber;
 
 public class DoubleObservableService {
 
-
-
     public Observable<String> getContent(){
-//
-//        return Observable.create(new Observable.OnSubscribe<String>() {
-//            @Override
-//            public void call(Subscriber<? super String> subscriber) {
-//                new Thread(new Runnable() {
-//                    @Override
-//                    public void run() {
-//                        subscriber.onNext("First Value");
-//                        subscriber.onNext("Second Value");
-//                        subscriber.onCompleted();
-//                    }
-//                }).start();
-//
-//            }
-//        });
         return Observable.just("First Value", "Second Value");
     }
-
 }
